b0ecb25d4ea1dc418698c01d9edf6e7824574650
philipborg <philip.borgstrom@gmail.com>
2015-08-11 21:17:27 +0200

    Added extra info
    Added extra info regarding double linked assets when updating project
    files

--- Create IDE Project files.bat
+++ Create IDE Project files.bat
@@ -4,4 +4,5 @@ title Create IDE Project Files
 echo This requires Java Development Kit 8 and Android SDK level 22
 pause
 call gradlew --no-daemon clean eclipse afterEclipseImport
+echo If you used this to update your project files the assets may be double linked, simply remove one
 pause
