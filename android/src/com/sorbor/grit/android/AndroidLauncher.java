ddfd05b9018ae8136533cfe84068d7999b89d62e
philipborg <philip.borgstrom@gmail.com>
2015-08-11 10:42:55 +0200

    Added Project

---
+++ android/src/com/sorbor/grit/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.sorbor.grit.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.sorbor.grit.Grit;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new Grit(), config);
+	}
+}
